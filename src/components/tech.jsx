f27050b030ed1fcd2c03bcccfb2cacbfe8769b81
Andrei <jackparquez1@gmail.com>
2025-02-15 15:36:10 +0800

    up

--- src/components/tech.jsx
+++ src/components/tech.jsx
@@ -4,7 +4,7 @@ import { SiTailwindcss, SiUnity, SiMysql } from 'react-icons/si';
 
 const techStacks = [
   { name: 'React.js', icon: <FaReact className="text-gray-500 group-hover:text-blue-500" /> },
-  { name: 'JavaScript', icon: <FaJsSquare className="text-gray-500 group-hover:text-yellow-500" /> },
+  { name: 'JS', icon: <FaJsSquare className="text-gray-500 group-hover:text-yellow-500" /> },
   { name: 'HTML', icon: <FaHtml5 className="text-gray-500 group-hover:text-orange-500" /> },
   { name: 'CSS', icon: <FaCss3Alt className="text-gray-500 group-hover:text-blue-700" /> },
   { name: 'Tailwind', icon: <SiTailwindcss className="text-gray-500 group-hover:text-teal-500" /> },
@@ -21,12 +21,12 @@ function Tech() {
       {techStacks.map((tech, index) => (
         <div
           key={index}
-          className="group flex flex-col items-center justify-center px-4 py-2 ring-zinc-700 ring-2 rounded-lg transform transition-transform duration-300 hover:bg-zinc-900 sm:px-2 sm:py-1"
+          className="group flex flex-col items-center justify-center px-4 py-2 ring-zinc-700 ring-2 rounded-lg transform transition-transform duration-300 hover:bg-zinc-900"
         >
-          <div className="flex items-center justify-center w-16 h-16 text-4xl transition-transform duration-300 group-hover:-translate-y-2 sm:w-12 sm:h-12 sm:text-3xl">
+          <div className="flex items-center justify-center w-12 h-12 text-3xl transition-transform duration-300 group-hover:-translate-y-2">
             {tech.icon}
           </div>
-          <p className="text-zinc-500 text-xs transition-colors duration-300 group-hover:text-white sm:text-xxs">{tech.name}</p>
+          <p className="text-zinc-500 text-xs transition-colors duration-300 group-hover:text-white">{tech.name}</p>
         </div>
       ))}
     </div>
