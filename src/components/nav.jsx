492638759fe37913c19a4f99c1ebe38f1270a608
Andrei <jackparquez1@gmail.com>
2025-02-17 09:33:03 +0800

    up

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -2,19 +2,24 @@ import React from 'react';
 
 const Nav = () => {
   return (
-    <nav className="flex items-center justify-between p-6  text-white  text-sm ">
-      <div className="text-xl font-bold">
-        andrei 
-      </div>
+    <nav className="flex items-center justify-between p-6 text-white text-sm">
+      <div className="text-xl font-bold">andrei</div>
       <ul className="flex space-x-4">
         <li>
-          <a href="#home" className="text-gray-300 hover:text-gray-400">projects</a>
+          <a href="#home" className="text-gray-300 hover:text-gray-400">Projects</a>
         </li>
         <li>
-          <a href="#about" className="text-gray-300 hover:text-gray-400">contact</a>
+          <a href="#about" className="text-gray-300 hover:text-gray-400">Contact</a>
         </li>
         <li>
-          <a href="#contact" className="text-gray-100 font-bold hover:text-blue-600">CV</a>
+          <a 
+            href="/resume.pdf" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            className="text-gray-100 font-bold hover:text-blue-600"
+          >
+            CV
+          </a>
         </li>
       </ul>
     </nav>
