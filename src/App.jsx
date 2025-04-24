157bcb112ecbd9b3e59b466d10bce1c08de369e3
Y4too <119648889+Y4too@users.noreply.github.com>
2025-04-24 12:08:08 +0800

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,23 @@
-
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import './App.css';
-import ParticleBg from './components/particlebg';
-import Nav from './components/nav';
-import Home from './components/home';
-import Project from './components/projects';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import "./App.css";
+import ParticleBg from "./components/particlebg";
+import Nav from "./components/nav";
+import Home from "./components/home";
+import Project from "./components/projects";
 import { Helmet } from "react-helmet";
-
-<Helmet>
-  <link rel="canonical" href="https://andreidev.netlify.app/" />
-</Helmet>
-
-
-
 
 function App() {
   return (
     <Router>
+      <Helmet>
+        <link rel="canonical" href="https://andreidev.netlify.app/" />
+      </Helmet>
       <div className="w-full sm:w-full md:w-1/2 lg:w-1/2 xl:w-1/3 2xl:1/3  mx-auto">
         <ParticleBg id="particles" />
         <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
-         
+
           <Route path="/projects" element={<Project />} />
         </Routes>
       </div>
